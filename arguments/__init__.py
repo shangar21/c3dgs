cd63a567c747f9e95520dc3ed45f56ba64b7a1df
GabrielWu2004 <gabrielwu2004@gmail.com>
2024-08-26 20:12:04 -0400

    clone and prune done 🗿🗿🗿

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -113,7 +113,7 @@ class OptimizationParams(ParamGroup):
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
         self.densify_until_iter = 15_000
-        self.densify_grad_threshold = 0.0002
+        self.densify_grad_threshold = 0.2
         self.random_background = False
         self.not_quantization_aware = False
         super().__init__(parser, "Optimization Parameters")
